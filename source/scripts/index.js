a4a1ccb07c9843027334821c61bc59aa21a64c94
margarita0206 <158091165+margarita0206@users.noreply.github.com>
2024-05-19 13:00:35 +0300

    Декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-header__toggle');
+
+navMain.classList.remove('main-header__container--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-header__container--closed')) {
+    navMain.classList.remove('main-header__container--closed');
+    navMain.classList.add('main-header__container--opened');
+  } else {
+    navMain.classList.add('main-header__container--closed');
+    navMain.classList.remove('main-header__container--opened');
+  }
+});
