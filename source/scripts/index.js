b5e511273bf2701c97f9316d112efc1704a10614
margarita0206 <158091165+margarita0206@users.noreply.github.com>
2024-05-30 11:41:12 +0300

    Отправляет проект на проверку

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,15 +1,18 @@
 /* в этот файл добавляет скрипты*/
-const navMain = document.querySelector('.main-nav');
+const navMain = document.querySelector('.main-header__nav');
 const navToggle = document.querySelector('.main-header__toggle');
 
-navMain.classList.remove('main-header__container--nojs');
+navToggle.classList.remove('main-header__toggle--nojs');
+navMain.classList.add('main-header__nav--closed');
 
 navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-header__container--closed')) {
-    navMain.classList.remove('main-header__container--closed');
-    navMain.classList.add('main-header__container--opened');
+  if (navMain.classList.contains('main-header__nav--closed')) {
+    navMain.classList.remove('main-header__nav--closed');
+    navMain.classList.add('main-header__nav--opened');
+    navToggle.classList.add('main-header__toggle--closed');
   } else {
-    navMain.classList.add('main-header__container--closed');
-    navMain.classList.remove('main-header__container--opened');
+    navMain.classList.add('main-header__nav--closed');
+    navMain.classList.remove('main-header__nav--opened');
+    navToggle.classList.remove('main-header__toggle--closed');
   }
 });
